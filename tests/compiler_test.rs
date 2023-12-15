fb25de6c5e6faef869f88fc8d50eb2ff5f261794
David Isaksson <davidisaksson93@gmail.com>
2023-12-15 21:25:20 +0100

    Add end to end test for the compiler

---
+++ tests/compiler_test.rs
@@ -0,0 +1,30 @@
+use mos6502::assembler::compiler::Compiler;
+
+#[test]
+fn test_basic() {
+    let input = "
+  LDX #$00
+  LDY #$00
+firstloop:
+  TXA
+  STA $0200,Y
+  PHA
+  INX
+  INY
+  CPY #$10
+  BNE firstloop ;loop until Y is $10
+secondloop:
+  PLA
+  STA $0200,Y
+  INY
+  CPY #$20      ;loop until Y is $20
+  BNE secondloop
+";
+    let mut compiler = Compiler::new(0x0600);
+    let bytes = compiler.compile(input);
+    let expected = [
+        0xa2, 0x00, 0xa0, 0x00, 0x8a, 0x99, 0x00, 0x02, 0x48, 0xe8, 0xc8, 0xc0, 0x10, 0xd0, 0xf5,
+        0x68, 0x99, 0x00, 0x02, 0xc8, 0xc0, 0x20, 0xd0, 0xf7,
+    ];
+    assert_eq!(bytes, expected);
+}
