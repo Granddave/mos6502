9ade86525a7ed20fff763f79816e662960d8aed3
David Isaksson <davidisaksson93@gmail.com>
2023-12-31 12:33:48 +0100

    example: Add a loop.asm example

---
+++ examples/loop.asm
@@ -0,0 +1,23 @@
+define counter $00  ; Address of counter
+define max 10       ; Num of times to loop
+
+main:
+  jsr init          ; Initialize counter
+  jsr loop          ; Run loop
+  jmp exit          ; Exit program
+
+init:
+  lda #0            ; Load 0 into accumulator
+  sta counter       ; Store accumulator into counter address
+  rts
+
+loop:
+  lda counter       ; Load what's in counter address into accumulator
+  cmp #max          ; Compare accumulator to max value
+  beq done          ; If equal, branch to done
+  inc counter       ; Increment counter
+  jmp loop          ; Jump to loop
+done:
+  rts               ; Return back to main
+
+exit:
