307e69caa4822c14857f8982394ac0f6f5e8bd56
David Isaksson <davidisaksson93@gmail.com>
2024-01-13 20:56:49 +0100

    bin: Fix clippy warning about return value

--- src/bin/mos6502.rs
+++ src/bin/mos6502.rs
@@ -39,7 +39,8 @@ enum Command {
 pub fn trace() -> FlushGuard {
     let (chrome_layer, _guard) = ChromeLayerBuilder::new().build();
     tracing_subscriber::registry().with(chrome_layer).init();
-    return _guard;
+
+    _guard
 }
 
 fn main() -> Result<()> {
