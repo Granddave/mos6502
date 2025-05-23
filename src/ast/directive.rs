a31773fa1e5ad13c27777a75514e946896e0369c
David Isaksson <davidisaksson93@gmail.com>
2025-05-23 21:37:32 +0200

    ast: Improve docs

--- src/ast/directive.rs
+++ src/ast/directive.rs
@@ -1,11 +1,12 @@
 use std::fmt;
 
+/// Commands to the assembler that are not instructions.
 #[derive(Debug, PartialEq, Clone)]
 pub enum Directive {
     /// Defines where in the memory code from now on should be placed.
     ///
-    /// E.g. `.org $8000` in the assembly code. What this means is that the code from now on should
-    /// be placed at address $8000.
+    /// E.g. `.org $8000` in the assembly code. Instructs the code generator that code from now on
+    /// should be placed at address $8000.
     ///
     /// The .org directive affects
     ///   - where the code from now on should be placed in the memory
